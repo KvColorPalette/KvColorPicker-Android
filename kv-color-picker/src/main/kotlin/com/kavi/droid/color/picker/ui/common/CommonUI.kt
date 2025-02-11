8efb78d699818930e3e6c4d0f990316c1e8d5ad0
Kavimal Wijewardana <kavimalw@gmail.com>
2025-02-11 11:10:10 -0500

    Created the selected color detail view on color picker

--- kv-color-picker/src/main/kotlin/com/kavi/droid/color/picker/ui/common/CommonUI.kt
+++ kv-color-picker/src/main/kotlin/com/kavi/droid/color/picker/ui/common/CommonUI.kt
@@ -1,20 +1,25 @@
 package com.kavi.droid.color.picker.ui.common
 
 import android.icu.text.DecimalFormat
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
@@ -28,7 +33,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -118,7 +127,6 @@ fun AlphaSlider(
             valueRange = 0f..1f,
             modifier = Modifier.weight(.8f),
         )
-
         Text(
             text = DecimalFormat("#.##").format(valueState.value).toString(),
             modifier = Modifier
@@ -133,17 +141,68 @@ fun AlphaSlider(
 
 @Composable
 fun SelectedColorDetail(
-    color: Color
+    color: Color,
+    colorHex: MutableState<TextFieldValue>
 ) {
+    // Retrieve a ClipboardManager object
+    val clipboardManager = LocalClipboardManager.current
+
     Row {
         // Display the current color in a Box with a MaterialTheme shape
-        Box(
+        Column (
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .height(75.dp)
+                    .width(75.dp)
+                    .background(color, shape = MaterialTheme.shapes.large)
+                    .border(2.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(12.dp))
+            )
+        }
+
+        Column (
             modifier = Modifier
-                .padding(12.dp)
-                .height(75.dp)
-                .width(75.dp)
-                .background(color, shape = MaterialTheme.shapes.large)
-                .border(2.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(12.dp))
-        )
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Text(
+                text = "Some Text Detail description",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Black
+            )
+
+            Row {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .weight(.6f),
+                    value = colorHex.value,
+                    maxLines = 1,
+                    label = { Text(text = "Color Hex") },
+                    onValueChange = { newColorHex ->
+                        colorHex.value = newColorHex
+                    }
+                )
+
+                Button(
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, top = 10.dp)
+                        .height(55.dp)
+                        .weight(.4f),
+                    shape = RoundedCornerShape(8.dp),
+                    onClick = {
+                        clipboardManager.setText(colorHex.value.annotatedString)
+                    }
+                ) {
+                    Text(text = "Copy")
+                }
+            }
+        }
     }
 }
--- kv-color-picker/src/main/kotlin/com/kavi/droid/color/picker/ui/pickers/RBGAColorPicker.kt
+++ kv-color-picker/src/main/kotlin/com/kavi/droid/color/picker/ui/pickers/RBGAColorPicker.kt
@@ -3,36 +3,24 @@ package com.kavi.droid.color.picker.ui.pickers
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,11 +39,7 @@ fun KvColorPicker(modifier: Modifier = Modifier, onColorSelected: (selectedColor
     val blue = rememberSaveable { mutableFloatStateOf(0f) }
     val alpha = rememberSaveable { mutableFloatStateOf(1f) }
 
-    var colorHex by remember { mutableStateOf(TextFieldValue("")) }
-    var hasTextFieldFocus by remember { mutableStateOf(false) }
-
-    // Retrieve a ClipboardManager object
-    val clipboardManager = LocalClipboardManager.current
+    val colorHex = remember { mutableStateOf(TextFieldValue("")) }
 
     // Derived state for the color based on RGBA values
     val color by remember {
@@ -66,7 +50,7 @@ fun KvColorPicker(modifier: Modifier = Modifier, onColorSelected: (selectedColor
 
     // Launch an effect to invoke the provided callback with the selected color
     LaunchedEffect(color) {
-        colorHex = TextFieldValue(ColorUtil.getHex(color = color))
+        colorHex.value = TextFieldValue(ColorUtil.getHex(color = color))
         onColorSelected.invoke(color)
     }
 
@@ -106,49 +90,7 @@ fun KvColorPicker(modifier: Modifier = Modifier, onColorSelected: (selectedColor
             }
         }
 
-        SelectedColorDetail(color = color)
-
-//        Row (
-//            modifier = Modifier
-//                .fillMaxWidth(),
-//            horizontalArrangement = Arrangement.Center,
-//            verticalAlignment = Alignment.CenterVertically
-//        ){
-//            OutlinedTextField(
-//                modifier = Modifier
-//                    .padding(1.dp)
-//                    .weight(1f)
-//                    .onFocusChanged {
-//                        hasTextFieldFocus = it.isFocused
-//                    },
-//                value = colorHex,
-//                maxLines = 1,
-//                label = { Text(text = "Color Hex") },
-//                onValueChange = { newColorHex ->
-//                    colorHex = newColorHex
-//                }
-//            )
-//
-//            Button(
-//                modifier = Modifier
-//                    .padding(start = 8.dp, end = 8.dp, top = 8.dp)
-//                    .height(50.dp)
-//                    .weight(1f),
-//                    shape = RoundedCornerShape(8.dp),
-//                    onClick = {
-//                        if (hasTextFieldFocus) {
-//                            val setColor = ColorUtil.getColorFromHex(colorHex.text)
-//                            red.floatValue = setColor.red
-//                            green.floatValue = setColor.green
-//                            blue.floatValue = setColor.blue
-//                        } else {
-//                            clipboardManager.setText(colorHex.annotatedString)
-//                        }
-//                    }
-//            ) {
-//                Text(text = if(hasTextFieldFocus) "Set" else "Copy")
-//            }
-//        }
+        SelectedColorDetail(color = color, colorHex = colorHex)
     }
 }
 
